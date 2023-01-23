efb7660baa42cf3ae348bd691827c49d258ba13b
AndrewKaranu <119486464+AndrewKaranu@users.noreply.github.com>
2023-01-23 17:12:10 +0300

    Added variables with multiple assignment

---
+++ variables.py
@@ -0,0 +1,11 @@
+# comments are used for describing our code
+# Multiple assignment
+a = b = c = 10
+print(a)
+print(b)
+print(c)
+# You can assign different values for each variable within the same line
+x, y, z = 1, 2, 3
+print(x)
+print(y)
+print(z)
