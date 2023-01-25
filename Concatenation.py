2665ec121bcdd74a59580fbeb685e72275f40930
AndrewKaranu <119486464+AndrewKaranu@users.noreply.github.com>
2023-01-25 17:48:42 +0300

    Created excercise.py and concatenation.py as well as added to strings.py

---
+++ Concatenation.py
@@ -0,0 +1,24 @@
+#Concatenation -> joining two or more strings 
+# It is done using the addition (+) operator
+# first_name = "Andrew"
+# last_name = "Karanu"
+# print(first_name + " " + last_name)
+
+# first_name = input("Enter your first name: ")
+# last_name = input("Enter your second name: ")
+# print(first_name + " " + last_name)
+
+# string formatting
+# print(first_name + " " + last_name)
+
+# # f-string
+# print(f"My name is: {first_name} {last_name}.")
+
+
+# format()
+# print("My name is: {first} {last}.".format( last=last_name, first=first_name))
+# first_name = "Andrew"
+# last_name = "Karanu"
+# user_names = input("please....: ")
+# user_names = user_names.split(",")
+# print(user_names[0].capitalize() + " " + user_names[1].capitalize())
--- strings.py
+++ strings.py
@@ -104,3 +104,59 @@ print(string4.strip("#"))
 
 # 9. replace() -> replaces a specified character with a given character
 print(string3.replace(" ", ""))
+
+# Create a variable and store your name in small letter. chage your name to have the first letter in caps
+# 10. Capitalize
+string5 = "andrew"
+print(string5.capitalize())
+
+# 11. split() -> splits a string into substring and returns a list
+string6 = "cissco,juniper,hp,avaya,nortel"
+
+print(string6)
+print(string6)
+print(len(string6))
+string6 = string6.split(",")
+print(string6)
+print(len(string6))
+
+# 12. join() -> Allows you to separate characters in a string with your character of choice
+string7 = "Cisco Switch"
+print("_".join(string7))
+print("#".join(string6))
+
+# 13. string slicing -> allowsyou to cut a certain specified portion of a string
+
+string8 = "0 E2 10.110.8.9 [160/5] via 10.119.254.6, 0:01:00 Ethernet2"
+print(string8[5:15])
+print(string8.index("1"))
+print(string8.find("t2"))
+print(len(string8))
+print(string8[5:])  #if you dont specify the last character it goes to the end
+print(string8[:11]) #if you dont specify the first character it starts from the first character in the string
+print(string8[:])  #if you dont specify anything it covers the whole string
+print(string8[:-48]) 
+
+print(string8[::2]) #This prints every other character in the string
+# Print hre2
+print(string8[52:59:2])
+
+print(string8[::2])
+sliced = string8[::2]
+
+print(sliced[-4:])
+# Change hre2 into CRE
+sliced2 = sliced[-4:-1]
+   
+print(sliced2.replace("h", "C").upper())
+Sliced3 = sliced2.replace("h", "C").upper()
+# Change CRE into C.R.E
+
+print(".".join(Sliced3))
+
+print(".".join(sliced2.replace("h", "C").upper())) #All the code in one line
+
+# 14. Printing a string in reverse order usimg slicing and step
+# By setting the step to -1 we can reverse the string
+
+print(string8[::-1])
